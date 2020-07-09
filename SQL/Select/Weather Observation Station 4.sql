53c58ee4bee32c0d0b0f32745f93f5f317068a54
rnhappysmile <rnhappysmile@gmail.com>
2020-07-10 00:00:09 +0900

    DISTINCT를 사용하여 중복 제거

---
+++ SQL/Select/Weather Observation Station 4.sql
@@ -0,0 +1,27 @@
+-- Weather Observation Station 4.sql
+
+/*
+
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru', there are 2 different city names: 'New York' and 'Bengalaru'. The query returns , because
+
+*/
+
+--Solution
+
+SELECT COUNT(CITY) - COUNT (DISTINCT City)
+FROM STATION ;
